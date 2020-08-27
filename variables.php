e49ac33ac9fbddb38dfead3fed897c06ca424dd3
Dalitso <dkasonde11@gmail.com>
2020-08-27 04:33:43 +0200

    initial commit and variables

---
+++ variables.php
@@ -0,0 +1,37 @@
+<?php 
+
+//  Basic printing 
+
+    // echo 'hello world';
+    // print 'hello world'; 
+
+    // variables 
+     
+    // strings 
+    $string1 = "Hello";
+    $string2 = "World";
+    $result = "hello World";
+    $greeting = $string1 . ' ' . $string2;
+    $greeting2 ="$string1 $string2";
+
+    $string3 = 'They\'re Here';
+
+    $num1 = 4;
+    $num2= 12;
+    $sum = $num1 + $num2;
+
+    $float1 = 2.1;
+    $bool1 = true;
+
+
+    // echo $result;
+
+    // echo $sum; 
+
+    // echo $greeting;
+    // echo $greeting2;
+
+    echo $string3;
+
+
+?>
