8f3fc99ddb27a36fc3aa7cbb9b8f9be95ae24d80
Dalitso <dkasonde11@gmail.com>
2020-08-29 04:11:47 +0200

    string functiojn

---
+++ string_functions.php
@@ -0,0 +1,23 @@
+<?php 
+
+    // $output = substr('Hello', 1, 3);
+    // $output = substr('Hello', -2);
+    // echo $output;
+
+
+    // $output = strlen("Hello World");
+    // echo $output;
+
+    // $output = strpos('Hello World', 'o');
+    // echo $output
+
+    // $output = strrpos('Hello World', 'o');
+    // echo $output
+
+    $text = 'Hello World          ';
+    // var_dump($text);
+
+    $trimmed = trim($text);
+    var_dump($trimmed);
+
+?>
