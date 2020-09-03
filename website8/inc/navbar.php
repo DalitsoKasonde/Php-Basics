983f83752b80a3de35622db8ebaf27078932da7a
Dalitso <dkasonde11@gmail.com>
2020-09-03 20:09:50 +0200

    final commit -m

--- website8/inc/navbar.php
+++ website8/inc/navbar.php
@@ -1,17 +1,18 @@
 <nav class="navbar navbar-default">
     <div class="container">
         <div class="navbar-header">
-            <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <!-- <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
-            </button>
+            </button> -->
             <a href="#" class="navbar-brand">PHP Blog</a>
         </div>
-        <div class="collapse navbar-collapse" id="navbar">
+        <div class="" id="navbar">
             <ul class="nav navbar-nav">
                 <li><a href="<?php echo ROOT_URL; ?>">Home</a></li>
+                <li><a href="<?php echo ROOT_URL; ?>addpost.php">Add Post</a></li>
             </ul>
         </div>
     </div>
--- website8/post.php
+++ website8/post.php
@@ -1,40 +1,53 @@
 <?php
+	require('config/config.php');
+	require('config/db.php');
 
-require('config/config.php');
-require('config/db.php');
+	// Check For Submit
+	if(isset($_POST['delete'])){
+		// Get form data
+		$delete_id = mysqli_real_escape_string($conn, $_POST['delete_id']);
 
-// get ID 
-$id = mysqli_real_escape_string($conn, $_GET['id']);
+		$query = "DELETE FROM posts WHERE id = {$delete_id}";
 
-// create query 
-$query = 'SELECT * FROM posts WHERE id = '.$id;
+		if(mysqli_query($conn, $query)){
+			header('Location: '.ROOT_URL.'');
+		} else {
+			echo 'ERROR: '. mysqli_error($conn);
+		}
+	}
 
-// get result 
-$result = mysqli_query($conn, $query);
+	// get ID
+	$id = mysqli_real_escape_string($conn, $_GET['id']);
 
-// fetch data 
-$post = mysqli_fetch_assoc($result);
-// var_dump($posts);
+	// Create Query
+	$query = 'SELECT * FROM posts WHERE id = '.$id;
 
-// free result 
-mysqli_free_result($result);
+	// Get Result
+	$result = mysqli_query($conn, $query);
 
-// close connection 
-mysqli_close($conn);
+	// Fetch Data
+	$post = mysqli_fetch_assoc($result);
+	//var_dump($posts);
 
+	// Free Result
+	mysqli_free_result($result);
+
+	// Close Connection
+	mysqli_close($conn);
 ?>
 
-<?php include('inc/header.php'); ?>
+	<?php include('inc/header.php'); ?>
+		<div class="container">
+			<a href="<?php echo ROOT_URL; ?>" class="btn btn-default">Back</a>
+			<h1><?php echo $post['title']; ?></h1>	
+			<small>Created on <?php echo $post['created_at']; ?> by <?php echo $post['author']; ?></small>
+			<p><?php echo $post['body']; ?></p>
+			<hr>
+			<form class="float-right" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+				<input type="hidden" name="delete_id" value="<?php echo $post['id']; ?>">
+				<input type="submit" name="delete" value="Delete" class="btn btn-danger">
+			</form>
 
-    <div class="container">
-        <a href="<?php echo ROOT_URL; ?>" class="btn btn-default">Back</a>
-    <h1><?php echo $post['title']; ?></h1>
-            <small>
-                Created on <?php echo $post['created_at']; ?>
-                By <?php echo $post['author']; ?>
-            </small>
-            <p>
-                <?php echo $post['body']; ?>
-            </p>
-     </div>
-     <?php include('inc/footer.php'); ?>
+			<a href="<?php echo ROOT_URL; ?>editpost.php?id=<?php echo $post['id']; ?>" class="btn btn-default">Edit</a>
+		</div>
+	<?php include('inc/footer.php'); ?>
