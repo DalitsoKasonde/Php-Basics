9285ad7510ae5e3c9131d49748084837067912aa
Dalitso <dkasonde11@gmail.com>
2020-09-03 15:51:12 +0200

    fetching data

---
+++ website8/index.php
@@ -0,0 +1,45 @@
+<?php
+
+require('config/config.php');
+require('config/db.php');
+
+// create query 
+$query = 'SELECT * FROM posts';
+
+// get result 
+$result = mysqli_query($conn, $query);
+
+// fetch data 
+$posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+// var_dump($posts);
+
+// free result 
+mysqli_free_result($result);
+
+// close connection 
+mysqli_close($conn);
+
+?>
+
+<?php include('inc/header.php'); ?>
+
+    <div class="container">
+    <h1>Posts</h1>
+    <?php foreach ($posts as $post) : ?>
+        <div class="well">
+            <h3>
+                <?php echo $post['title']; ?>
+            </h3>
+            <small>
+                Created on <?php echo $post['created_at']; ?>
+                By <?php echo $post['author']; ?>
+            </small>
+            <p>
+                <?php echo $post['body']; ?>
+            </p>
+            <a href="<?php echo ROOT_URL; ?>post.php?id=<?php echo $post['id']; ?>" class="btn btn-default">Read more</a>
+        </div>
+    <?php endforeach; ?>
+    </div>
+
+    <?php include('inc/footer.php'); ?>
---
+++ website8/post.php
@@ -0,0 +1,40 @@
+<?php
+
+require('config/config.php');
+require('config/db.php');
+
+// get ID 
+$id = mysqli_real_escape_string($conn, $_GET['id']);
+
+// create query 
+$query = 'SELECT * FROM posts WHERE id = '.$id;
+
+// get result 
+$result = mysqli_query($conn, $query);
+
+// fetch data 
+$post = mysqli_fetch_assoc($result);
+// var_dump($posts);
+
+// free result 
+mysqli_free_result($result);
+
+// close connection 
+mysqli_close($conn);
+
+?>
+
+<?php include('inc/header.php'); ?>
+
+    <div class="container">
+        <a href="<?php echo ROOT_URL; ?>" class="btn btn-default">Back</a>
+    <h1><?php echo $post['title']; ?></h1>
+            <small>
+                Created on <?php echo $post['created_at']; ?>
+                By <?php echo $post['author']; ?>
+            </small>
+            <p>
+                <?php echo $post['body']; ?>
+            </p>
+     </div>
+     <?php include('inc/footer.php'); ?>
